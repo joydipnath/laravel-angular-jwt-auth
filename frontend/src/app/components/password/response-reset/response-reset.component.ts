c3e148a11aaf5dea425c8f1d62d2be2aede3f624
joydipnath <joydipsworld@gmail.com>
2019-06-07 21:46:43 +0530

    reset password is done, can update also, next send notification

--- frontend/src/app/components/password/response-reset/response-reset.component.ts
+++ frontend/src/app/components/password/response-reset/response-reset.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoginSignupService } from '../../../services/auth/login-signup.service';
 
 @Component({
@@ -19,6 +19,7 @@ export class ResponseResetComponent implements OnInit {
 
 	constructor(
 		private route: ActivatedRoute,
+		private router: Router,
 		private LoginSignup: LoginSignupService
 	
 	) { 
@@ -37,11 +38,12 @@ export class ResponseResetComponent implements OnInit {
 			error => this.handleError(error)
 		);
 	}
-	handleResponse(data){
 
+	handleResponse(data){
+		this.router.navigateByUrl('/login');
 	}
 
 	handleError(error){
-
+		return this.error = error.error.error;
 	}
 }
